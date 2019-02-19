f1916755029a36721a315ce97dd68f48656748ec
Christopher Rieder <christopher.rieder@gmail.com>
2019-02-19 13:18:21 -0300

    removing log to file

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ var logFile = fs.createWriteStream('log.txt', { flags: 'a' });
 var logStdout = process.stdout;
 
 console.log = function () {
-  logFile.write(util.format.apply(null, arguments) + '\n');
+  // logFile.write(util.format.apply(null, arguments) + '\n');
   logStdout.write(util.format.apply(null, arguments) + '\n');
 };
 console.error = console.log;
